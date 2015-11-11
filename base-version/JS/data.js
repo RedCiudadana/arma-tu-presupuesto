9c80ab203b33a4e0acb53443a10557a84cb21bd8
Hector Hurtarte <hectorh30@gmail.com>
2015-11-11 00:53:54 -0600

    Modificar descripción de catgorías de presupuesto

--- base-version/JS/data.js
+++ base-version/JS/data.js
@@ -17,62 +17,69 @@ function InfoCategory(cat)
 	switch(cat)
 	{
 		case 'Servicios públicos generales':
-			return "La ciudad ofrece un sistema público y gratuito de servicios de "
-				+ "salud que incluye: atención en hospitales y centros de atención, "
-				+ "acciones de prevención y la acción del SAME, entre otros. El "
-				+ "Ministerio de Salud dispone del 90% de esta área.";
-				break;
+			return "Órganos ejecutivos y legislativos, asuntos financieros y fiscales, asuntos "
+				+ "exteriores, Ayuda económica exterior, Transacciones de la deuda pública y "
+				+ "Transferencias de carácter general entre diferentes niveles de gobierno";
+			break;
+
 		case 'Defensa':
-			return "¿Cómo garantizar la igualdad de oportunidades? Hoy, Buenos Aires "
-				+ "ofrece, por ejemplo, el programa Ciudadanía Porteña y estímulos "
-				+ "económicos para la finalización de estudios secundarios. ¿Vos "
-				+ "qué harías? El 50% de este presupuesto lo ejecuta el Ministerio "
-				+ "de Desarrollo Social.";
-				break;
+			return "Defensa militar, Defensa civil, Ayuda militar al exterior, Investigación "
+				+ "y desarrollo relacionados con la defensa y Defensa";
+			break;
+
 		case 'Orden público y seguridad':
-			return "¿Cómo nos trasladamos en la ciudad? Metrobuses, ciclovías, políticas "
-				+ "de reordenamiento del tránsito y de promoción de la seguridad "
-				+ "vial: todo esto es la política de transporte de la ciudad. En "
-				+ "la actualidad, el 80 % lo implementa la Jefatura De Gabinete De Ministros.";
-				break;
+			return "Servicios de policía, Servicios de protección contra incendios, Tribunales "
+				+ "de justicia, Prisiones, Investigación y desarrollo relacionados con el "
+				+ "orden público y la seguridad y Orden público y seguridad";
+			break;
+
 		case 'Asuntos económicos':
-			return "El funcionamiento de los programas y actividades de una ciudad "
-				+ "requieren de personas que los lleven a cabo! Aquí incluimos los "
-				+ "salarios que paga la Ciudad a técnicos, profesionales, legisladores "
-				+ "y demás miembros de la administración pública porteña.";
-				break;
+			return "Asuntos económicos, comerciales y laborales en general, Agricultura, "
+				+ "silvicultura, pesca y caza, Combustibles y energía, Minería, "
+				+ "manufacturas y construcción, Transporte, Comunicaciones, Otras industrias, "
+				+ "Investigación y desarrollo relacionados con asuntos económicos";
+			break;
+
 		case 'Protección del medio ambiente':
-			return "En su Constitución, la Ciudad reconoce el derecho a una vivienda "
-				+ "digna. Hoy el Instituto de la Vivienda (Ministerio de Desarrollo "
-				+ "Urbano) implementa el programa Mi casa BA y Alquilar se Puede, "
-				+ "además de impulsar la urbanización de villas.";
-				break;
+			return "Ordenación de desechos, Ordenación de aguas residuales, Reducción de la "
+				+ "contaminación, Protección de la diversidad biológica y del paisaje, "
+				+ "Investigación y desarrollo relacionados con la protección del medio "
+				+ "ambiente y Protección del medio ambiente";
+			break;
+
 		case 'Vivienda y servicios comunitarios':
-			return "Recolección y clasificación de residuos, mantenimiento del sistema "
-				+ "de alcantarillado y de los espacios verdes: los Ministerios de "
-				+ "Ambiente y Espacio Público y de Desarrollo Urbano impulsan estas "
-				+ "tareas. ¿Hay más por hacer?";
-				break;
+			return "Urbanización, Desarrollo comunitario, Abastecimiento de agua, Alumbrado "
+				+ "público, Investigación y desarrollo relacionados con la vivienda y los "
+				+ "servicios comunitarios, Vivienda y servicios comunitarios";
+			break;
+
 		case 'Salud':
-			return "Festivales de teatro, cine, danza, la actividad en los centros "
-				+ "culturales de los barrios, la promoción de artistas emergentes "
-				+ "y también el disfrute de la ciudad como polo turístico: todo esto "
-				+ "es parte de la tarea del Ministerio de Cultura.";
-				break;
+			return "Productos, útiles y equipo médicos, Servicios para pacientes "
+				+ "externos, Servicios hospitalarios, Servicios de salud pública, "
+				+ "Investigación y desarrollo relacionados con la salud y Salud";
+			break;
+
 		case 'Actividades recreativas, cultura y religión':
-			return "Más del 90% del presupuesto educativo es ejecutado por el Ministerio "
-				+ "de Educación. Se destina a brindar educación pública, laica y "
-				+ "gratuita en diferentes niveles: inicial, primaria, secundaria y superior.";
-				break;
+			return "Servicios recreativos y deportivos, Servicios culturales, "
+				+ "Servicios de radio y televisión y servicios editoriales, "
+				+ "Servicios religiosos y otros servicios comunitarios, "
+				+ "Investigación y desarrollo relacionados con esparcimiento, "
+				+ "cultura y religión, Actividades recreativas y cultura y religión";
+			break;
+
 		case 'Educación':
-			return "Créditos a emprendimientos, capacitaciones profesionales, fomento "
-				+ "a la industria cultural: son algunas de las líneas de trabajo "
-				+ "del Ministerio de Desarrollo Económico. ¿Qué harías vos?";
-				break;
+			return "Enseñanza preescolar y enseñanza primaria, Enseñanza secundaria, "
+				+ "Enseñanza postsecundaria no terciaria, Enseñanza terciaria, "
+				+ "Enseñanza no atribuible a ningún nivel, Servicios auxiliares de "
+				+ "la educación, Investigación y desarrollo relacionados con la educación "
+				+ "y Enseñanza";
+			break;
+
 		case 'Protección social':
-			return "La prevención del delito y su control constituyen tareas del Ministerio "
-				+ "de Justicia y Seguridad. La Policía Metropolitana, por ejemplo, "
-				+ "se financia con este presupuesto.";
-				break;
+			return "Enfermedad e incapacidad, Edad avanzada, Supérstites. "
+				+ "Familia e hijos, Desempleo, Vivienda, Exclusión social, "
+				+ "Investigación y desarrollo relacionados con la protección "
+				+ "social y Protección social";
+			break;
 	}
 }
